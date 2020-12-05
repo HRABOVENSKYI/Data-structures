4c4c54a08938c84adc0a39699f20b515d2218e74
Teodor Hrabovenskyi <teodor.gr.2002@gmail.com>
2020-12-05 12:34:48 +0200

    Level traversal with int elements in binary tree

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -61,6 +62,20 @@ int FindHeight(BstNode *root) {
     return max(FindHeight(root->left), FindHeight(root->right)) + 1;
 }
 
+void LevelOrderTraversal(BstNode *root) {
+    if (root == NULL) return;
+    queue<BstNode *> Q;
+    Q.push(root);
+    //while there is at least one discovered node
+    while (!Q.empty()) {
+        BstNode *current = Q.front();
+        cout << current->data << " ";
+        if (current->left != NULL) Q.push(current->left);
+        if (current->right != NULL) Q.push(current->right);
+        Q.pop(); //removing the element at front
+    }
+}
+
 int main() {
     BstNode *root = NULL; //creating an empty tree
     root = Insert(root, 15);
@@ -69,11 +84,17 @@ int main() {
     root = Insert(root, 25);
     root = Insert(root, 8);
     root = Insert(root, 12);
+    root = Insert(root, 6);
+    root = Insert(root, 32);
 
     cout << "Min element is: " << FindMin(root) << endl;
     cout << "Max element is: " << FindMax(root) << endl;
 
     cout << "Height of the tree is " << FindHeight(root) << endl;
+
+    cout << "Traversal in level order: ";
+    LevelOrderTraversal(root);
+    cout << endl;
 
     int number;
     cout << "Enter number to search" << endl;
