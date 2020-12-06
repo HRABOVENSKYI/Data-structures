7e4fac9aa0f0520cebf15366ce41272b80fc0d84
Teodor Hrabovenskyi <teodor.gr.2002@gmail.com>
2020-12-06 20:05:43 +0200

    Lab 8 works, working perfectly, with deletion from previous commits

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-vector<int> k;
-
 struct Node {
     string surname;
     string name;
@@ -15,6 +13,8 @@ struct Node {
     Node *left;
     Node *right;
 };
+
+vector<Node *> k;
 
 Node *InsertByRating(Node *root, string surname, string name, string group, int dateOfBirth, int rating) {
     if (root == NULL) {
@@ -55,95 +55,55 @@ Node *FindMin(Node *root) {
     return root;
 }
 
-void deleteDeepest(Node *root, Node *d_node) {
-    queue<Node *> q;
-    q.push(root);
-
-    // Do level order traversal until last node
-    Node *temp;
-    while (!q.empty()) {
-        temp = q.front();
-        q.pop();
-        if (temp == d_node) {
-            temp = NULL;
-            delete (d_node);
-            return;
-        }
-        if (temp->right) {
-            if (temp->right == d_node) {
-                temp->right = NULL;
-                delete (d_node);
-                return;
-            } else
-                q.push(temp->right);
+Node *Delete(Node *root, int rating) {
+    if (root == NULL) return root;
+    else if (rating < root->rating) root->left = Delete(root->left, rating);
+    else if (rating > root->rating) root->right = Delete(root->right, rating);
+    else { //Found this data
+        //case 1: No child
+        if (root->left == NULL && root->right == NULL) {
+            delete root;
+            root = NULL;
         }
 
-        if (temp->left) {
-            if (temp->left == d_node) {
-                temp->left = NULL;
-                delete (d_node);
-                return;
-            } else
-                q.push(temp->left);
+            //case 2: One child
+        else if (root->left == NULL) {
+            Node *temp = root;
+            root = root->right;
+            delete temp;
+        } else if (root->right == NULL) {
+            Node *temp = root;
+            root = root->left;
+            delete temp;
         }
-    }
-}
 
-// function to delete element in binary tree
-Node *DeleteByGroup(Node *root, string group) {
-    if (root == NULL)
-        return NULL;
-
-    if (root->left == NULL && root->right == NULL) {
-        if (root->group == group)
-            return NULL;
-        else
-            return root;
-    }
-
-    queue<Node *> q;
-    q.push(root);
-
-    Node *temp;
-    Node *key_node = NULL;
-
-    // Do level order traversal to find deepest
-    // node(temp) and node to be deleted (key_node)
-    while (!q.empty()) {
-        temp = q.front();
-        q.pop();
-
-        if (temp->group == group)
-            key_node = temp;
-
-        if (temp->left)
-            q.push(temp->left);
-
-        if (temp->right)
-            q.push(temp->right);
-    }
-
-    if (key_node != NULL) {
-        string x = temp->group;
-        deleteDeepest(root, temp);
-        key_node->group = x;
+            //case 3: 2 children
+        else {
+            Node *temp = FindMin(root->right);
+            root->name = temp->name;
+            root->surname = temp->surname;
+            root->group = temp->group;
+            root->dateOfBirth = temp->dateOfBirth;
+            root->rating = temp->rating;
+            root->right = Delete(root->right, temp->rating);
+        }
     }
     return root;
 }
 
-void NumOfGroupStudents(Node *root, string group) {
+void GroupStudents(Node *root, string group) {
     if (root == NULL) return;
-    NumOfGroupStudents(root->left, group);
+    GroupStudents(root->left, group);
     if (root->group == group) {
-        k.push_back(1);
+        k.push_back(root);
     }
-    NumOfGroupStudents(root->right, group);
+    GroupStudents(root->right, group);
 }
 
 //function to print nodes, where rating >= 75, in a binary tree inorder
-void InorderBestStudens(Node *root, int min) {
+void InorderBestStudents(Node *root, int min) {
     if (root == NULL) return;
-    InorderBestStudens(root->left, min);
+    InorderBestStudents(root->left, min);
     if (root->rating >= min) {
         cout << "Student: " << root->name << " " << root->surname << endl;
         cout << "Group: " << root->group << endl;
@@ -151,7 +111,7 @@ void InorderBestStudens(Node *root, int min) {
         cout << "Rating: " << root->rating << endl;
         cout << endl;
     }
-    InorderBestStudens(root->right, min);
+    InorderBestStudents(root->right, min);
 }
 
 void LevelOrderTraversal(Node *root) {
@@ -176,12 +136,13 @@ int main() {
     //creating an empty tree
     Node *root = NULL;
 
-    root = InsertByRating(root, "Hemsworth", "Chris", "IoT-11", 1983, 78);
-    root = InsertByRating(root, "Downey", "Robert", "IoT-12", 1965, 86);
-    root = InsertByRating(root, "Johansson", "Scarlett", "IoT-13", 1984, 70);
-    root = InsertByRating(root, "Ruffalo", "Mark", "IoT-12", 1967, 92);
-    root = InsertByRating(root, "Holland", "Tom", "IoT-11", 1996, 98);
-    root = InsertByRating(root, "Elizabeth", "Olsen", "IoT-13", 1989, 88);
+    //create binary tree, sorted by rating
+    root = InsertByRating(root, "Hemsworth", "Chris", "IoT-11", 1983, 88);
+    root = InsertByRating(root, "Downey", "Robert", "IoT-12", 1965, 78);
+    root = InsertByRating(root, "Johansson", "Scarlett", "IoT-13", 1984, 86);
+    root = InsertByRating(root, "Ruffalo", "Mark", "IoT-12", 1967, 70);
+    root = InsertByRating(root, "Holland", "Tom", "IoT-11", 1996, 92);
+    root = InsertByRating(root, "Elizabeth", "Olsen", "IoT-13", 1989, 98);
     root = InsertByRating(root, "Evans", "Chris", "IoT-11", 1981, 54);
 
     cout << "=========================================" << endl;
@@ -189,19 +150,18 @@ int main() {
     Inorder(root);
     cout << endl;
 
-    NumOfGroupStudents(root, "IoT-11");
+    GroupStudents(root, "IoT-12"); //create a vector of pointers to students from one group
     for (int i = 0; i < k.size(); i++) {
-        root = DeleteByGroup(root, "IoT-11");
+        root = Delete(root, k[i]->rating); //delete every student from this group
     }
     cout << "=========================================" << endl;
-    cout << "Print nodes in inorder, sorted by rating" << endl;
-    cout << "after deleting students of IoT-11:\n" << endl;
+    cout << "Print nodes in inorder, sorted by rating after deletion:\n" << endl;
     Inorder(root);
     cout << endl;
 
     cout << "=========================================" << endl;
     cout << "Print best students, sorted by rating:\n" << endl;
-    InorderBestStudens(root, 74);
+    InorderBestStudents(root, 74);
     cout << endl;
 
     cout << "=========================================" << endl;
